b36638245539b06e3cb5b142f657c775e8368bfa
Dmitrii Elkin <7777632@gmail.com>
2021-05-05 18:42:30 +0300

    Loading state

--- lib/screenPersonDetails.dart
+++ lib/screenPersonDetails.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_app/theme.dart';
 
 import 'api.dart';
 import 'models.dart';
@@ -19,17 +19,31 @@ class PersonDetailsScreen extends StatefulWidget {
 class _State extends State<PersonDetailsScreen> {
   final int id;
   PersonDetails? person;
+  bool loading = true;
 
   _State({required this.id}) : super();
 
   void load() async {
+    setState(() {
+      this.loading = true;
+    });
     try {
       final res = await loadPersonDetails(id);
       setState(() {
         this.person = res;
+        this.loading = false;
       });
     } on Exception {
-      EasyLoading.showError('Ошибка загрузки');
+      // new Future.delayed(Duration.zero, () {
+      //   ScaffoldMessenger.of(context).showSnackBar(
+      //     SnackBar(
+      //         content: Text('Fail to load data'),
+      //         backgroundColor: Colors.red[900]),
+      //   );
+      // });
+      setState(() {
+        this.loading = false;
+      });
     }
   }
 
@@ -44,63 +58,81 @@ class _State extends State<PersonDetailsScreen> {
         appBar: AppBar(
           title: Text(person == null ? '' : person!.name),
         ),
-        body: person == null
-            ? Center(child: Text('Please, wait...'))
-            : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Image.network(
-                      person!.avatar,
-                      fit: BoxFit.contain,
-                      width: double.infinity,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 16, left: 16),
-                      child: Row(
-                        children: [
-                          Text('Name: ', style: textBold),
-                          Text(person!.name, style: textNormal),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 12, left: 16),
-                      child: Row(
-                        children: [
-                          Text('Status: ', style: textBold),
-                          Text(person!.status, style: textNormal),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 12, left: 16),
-                      child: Row(
-                        children: [
-                          Text('Gender: ', style: textBold),
-                          Text(person!.gender, style: textNormal),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 12, left: 16),
-                      child: Row(
-                        children: [
-                          Text('Species: ', style: textBold),
-                          Text(person!.species, style: textNormal),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 12, left: 16),
-                      child: Row(
-                        children: [
-                          Text('Origin: ', style: textBold),
-                          Text(person!.originName, style: textNormal),
-                        ],
-                      ),
-                    ),
-                  ],
+        body: loading
+            ? Center(
+                child: CircularProgressIndicator(
+                  semanticsLabel: 'Loading',
+                  valueColor: AlwaysStoppedAnimation(ThemeColors.primary),
                 ),
-              ),
+              )
+            : person == null
+                ? Center(
+                    child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Fail to load data'),
+                      ElevatedButton(
+                          onPressed: load,
+                          child: Text('Try again'),
+                          style: ElevatedButton.styleFrom(
+                              primary: ThemeColors.primary)),
+                    ],
+                  ))
+                : SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Image.network(
+                          person!.avatar,
+                          fit: BoxFit.contain,
+                          width: double.infinity,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(top: 16, left: 16),
+                          child: Row(
+                            children: [
+                              Text('Name: ', style: textBold),
+                              Text(person!.name, style: textNormal),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(top: 12, left: 16),
+                          child: Row(
+                            children: [
+                              Text('Status: ', style: textBold),
+                              Text(person!.status, style: textNormal),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(top: 12, left: 16),
+                          child: Row(
+                            children: [
+                              Text('Gender: ', style: textBold),
+                              Text(person!.gender, style: textNormal),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(top: 12, left: 16),
+                          child: Row(
+                            children: [
+                              Text('Species: ', style: textBold),
+                              Text(person!.species, style: textNormal),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(top: 12, left: 16),
+                          child: Row(
+                            children: [
+                              Text('Origin: ', style: textBold),
+                              Text(person!.originName, style: textNormal),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
       );
 }
